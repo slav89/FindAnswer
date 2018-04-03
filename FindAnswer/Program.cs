bae67db60bbb2e87299515d16479416063500479
Slav <>
2018-04-02 22:27:21 -0500

    Initial commit

---
+++ FindAnswer/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using test;
+
+namespace FindAnswerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestParsing();
+            //return;
+
+            int i = 0;
+            while (true)
+            {
+                DirectoryInfo d = new DirectoryInfo(@"/Users/slav/Desktop/platform-tools/screens/hq/live");
+                FileInfo[] Files = d.GetFiles("*.png"); //Getting Text files
+
+                if (Files.Length == 1)
+                {
+                    Thread.Sleep(500);
+                    try
+                    {
+                        var then = DateTime.Now;
+                        ProcessScreenshot(i, Files[0].FullName);
+                        var took = (DateTime.Now - then).TotalMilliseconds;
+                        Console.WriteLine(took);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine($"{i}. Crashed");
+                    }
+                    File.Delete(Files[0].FullName);
+                    i++;
+                }
+            }
+        }
+
+        private static long RunSearch(string url)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest();
+
+            var result = client.Execute(request);
+            var content = JObject.Parse(result.Content);
+            var searchInfo = content["searchInformation"];
+            var totalResults = (long)searchInfo["totalResults"];
+            return totalResults;
+        }
+
+        private static void ProcessScreenshot(int i, string fileName){
+            string text;
+            try
+            {
+                text = OcrClient.Recognize(fileName);
+            }
+            catch (Exception)
+            {
+                text = OcrClient.Recognize(fileName);
+            }
+            var qa = new test.QuestionSplitter(text);
+
+            var question = qa.GetQuestion();
+            var questionForQuery = question;
+
+            var negative = false;
+            if (question.ToLower().Contains(" not "))
+            {
+                negative = true;
+                questionForQuery = question.Replace("not", "");
+            }
+
+            var a = qa.GetCaseA();
+            var b = qa.GetCaseB();
+            var c = qa.GetCaseC();
+
+            //quoates around cases - all words in case should be present
+            var queryA = $"{questionForQuery} \"{a}\"";
+            var queryB = $"{questionForQuery} \"{b}\"";
+            var queryC = $"{questionForQuery} \"{c}\"";
+
+            //no quotes
+            //var queryA = $"{questionForQuery} {a}";
+            //var queryB = $"{questionForQuery} {b}";
+            //var queryC = $"{questionForQuery} {c}";
+
+            var searchClient = new GoogleSearchClient();
+
+            var taskA = Task.Run<long>(() => searchClient.RunSearch(queryA));
+            var taskB = Task.Run<long>(() => searchClient.RunSearch(queryB));
+            var taskC = Task.Run<long>(() => searchClient.RunSearch(queryC));
+
+            var results = new Dictionary<string, long>();
+            results.Add($"A. {a}", taskA.Result);
+            results.Add($"B. {b}", taskB.Result);
+            results.Add($"C. {c}", taskC.Result);
+
+            var winner = negative
+                ? results.OrderByDescending(res => res.Value).LastOrDefault()
+                : results.OrderByDescending(res => res.Value).FirstOrDefault();
+            
+            Console.WriteLine(i + ". " + question + "?");
+            Console.WriteLine(winner.Key);
+
+            //For testing
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+
+            Console.WriteLine();
+        }
+
+        static void TestParsing()
+        {
+            var texts = new[]{
+                //"T-Mobile\nна\nRI .ill 4Đư 8:05 PM\nна\n10\nWhich of these is a\ncommon breed of dog?\nMarmite\nMalamute\nMermarn\ncarlooch17 She looks hotter than us\nFOR SURE\n, JayDar 589 new messages\nchristieorns Sne's just aragging it out to\n",
+                //"T-Mobile ψ на\n48 1 8:08 PM\n360K\nна\nWhich work by Roald Dahl\nwas adapted into a 3-act\nopera in 1998?\nMatilda\nJames & the Giant Peach\nThe Fantastic Mr. Fox\nerik123456789 Sarah is a Hottie\nBenga!Sam Awkkward\nmsbla 1440 new messages\n",
+                //"T-Mobile ț, на\nN\n.111 4Đư 8:09 PM\n214K\nна\nEliminated\nThe CEO of what teckh\ncompany turned heads in\n2013 by buying a funeral\nhome?\nTesla\nAirbnb\nYahoo\nKittycat2333 hi\nKviaChandler12 Tootie are you ont\nFresh i 1842 new messages\n",
+                //"T-Mobile Ψ на\nN 48 8:13 PM\n154K\nHO\nTime's up!\nWhich of these events\ntraditionally occurs on the\nTuesday after Easter in\nTobago?\nGoat race\nRabbit jumping contest\nParrot talent show\nfrodothebrave #notmyhost\nO bubby93 love u bb\nalmys 2975 new messages NIA!!!\nLITHUAINIAH\n",
+                "T-Mobile ț, на\nN\n48 1 8:12 PM\n158K\nHO\n5\nA photo of Meryl Streep\nyelling through her hands\nwas taken at what 2015\nawards show?\nOscars\nGolden Globes?\nSAG Awards\n罗evy.0.05 #NOTMYSCOTT\na mattdcontreras SCOTT IS THE\nWOO 2595 new messages YING\nwhippersnapper360 she looks really\n",
+                "4G\nT-Mobile M\nна\n10\nWhat hairstyle is named\nfor a part of an animal\nHorseholder\nBowl cut\nPonytail\nsillybear25 Ack!\nrkateri723 wheres the airmaxes!!!?\nmozzarxila bsidisidia\n"
+            };
+            foreach (string text in texts)
+            {
+                var qs = new QuestionSplitter(text);
+
+                var question = qs.GetQuestion();
+                var a = qs.GetCaseA();
+                var b = qs.GetCaseB();
+                var c = qs.GetCaseC();
+
+                Console.WriteLine($"{question}?");
+                Console.WriteLine($"A. {a}");
+                Console.WriteLine($"B. {b}");
+                Console.WriteLine($"C. {c}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ FindAnswer/QuestionSplitter.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace test
+{
+    public class QuestionSplitter
+    {
+        string _text;
+        string[] _cases = null;
+
+        public QuestionSplitter(string text)
+
+        {
+            _text = text;
+        }
+
+        public string GetQuestion()
+        {
+            return _text.ToLower().Contains("cashshow")
+                       ? GetQuestionCashShow()
+                            : GetQuestionHq();
+        }
+
+        public string GetQuestionCashShow()
+        {
+            Regex regex = new Regex("ound(.*)\\?", RegexOptions.Singleline);
+
+            Match match = regex.Match(_text);
+
+            Regex regex1 = new Regex("\n(.*)\\?", RegexOptions.Singleline);
+
+            var m = regex1.Match(match.Value.Split("\n", 2)[1]);
+            var question = m.Groups[1].Value;
+
+            var tidyquestion = question.Replace("\n", " ").Replace("-", "").Trim();
+
+            return tidyquestion;
+        }
+
+        public string GetQuestionHq()
+        {           
+            Regex regex = new Regex("\n[0-9][0-9]?\n(.*)\\?", RegexOptions.Singleline);
+            Match match = regex.Match(_text);
+
+            if (!match.Success)
+            {
+                regex = new Regex("\nна\n(.*)\\?", RegexOptions.Singleline);
+                match = regex.Match(_text);
+            }
+            if (!match.Success)
+            {
+                regex = new Regex("Eliminated\n(.*)\\?", RegexOptions.Singleline);
+                match = regex.Match(_text);
+            }
+            if (!match.Success)
+            {
+                regex = new Regex(" up!\n(.*)\\?", RegexOptions.Singleline);
+                match = regex.Match(_text);
+            }
+            if (!match.Success)
+            {
+                regex = new Regex("\n[0-9][0-9]?\n(.*l)\n", RegexOptions.Singleline);
+                match = regex.Match(_text);
+            }
+
+            var question = match.Groups[1].Value;
+
+            if (question.Equals(string.Empty))
+            {
+                Console.Beep();
+                return "";
+            }
+
+            if (question.Contains("?"))
+            {
+                question = question.Split("?", 2)[0];
+            }
+
+            if (question.StartsWith("Eliminated"))
+            {
+                question = question.Replace("Eliminated", "");
+            }
+
+            var tidyquestion = question.Replace("\n", " ").Replace("-", "").Trim();
+
+            return tidyquestion;
+        }
+
+        public string[] GetCases()
+        {
+            if (_cases == null)
+            {
+                Regex regex = new Regex("\\?\n([a-z,0-9, ]*\n.*\n.*)\n", RegexOptions.Singleline);
+                Match match = regex.Match(_text);
+
+                if (!match.Success)
+                {
+                    regex = new Regex("l\n([a-z,0-9, ]*\n.*\n.*)\n", RegexOptions.Singleline);
+                    match = regex.Match(_text);
+                }
+
+                var casesString = _text.Split("?", 2)[1];
+                _cases = casesString.Split("\n", 5);
+
+                if (_cases.Length < 5)
+                {
+                    casesString = _text.Split("l\n", 2)[1];
+                    casesString = "\n" + casesString;
+                    _cases = casesString.Split("\n", 5);
+                }
+            }
+            return _cases;
+        }
+
+        public string GetCaseA()
+        {
+            return GetCases()[1];
+        }
+
+        public string GetCaseB()
+        {
+            return GetCases()[2];
+        }
+
+        public string GetCaseC()
+        {
+            return GetCases()[3];
+        }
+    }
+}
