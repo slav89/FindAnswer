522064ebc4776ccc94fd320790f6204e60e421fd
Slav <>
2018-04-02 22:27:21 -0500

    Initial commit

---
+++ FindAnswer/GoogleSearchClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace test
+{
+    public class GoogleSearchClient
+    {
+        string _baseUri = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAlhl1TSHrJZpToLSgl3N1rOvYROncae7w" +
+                "&cx=013427879713822343421:hjfnay8em2e&q=";
+        string _baseUriRegular = "https://www.google.com/search?q=";
+
+        public GoogleSearchClient()
+        {
+        }
+
+        public long RunSearch(string query)
+        {
+            var searchUri = _baseUri + query;
+            var client = new RestClient(searchUri);
+            var request = new RestRequest();
+
+            var result = client.Execute(request);
+            var content = JObject.Parse(result.Content);
+            var searchInfo = content["searchInformation"];
+            var totalResults = (long)searchInfo["totalResults"];
+
+            return totalResults;
+        }
+
+        public long RunRegularSearch(string query)
+        {
+               var searchUri = _baseUriRegular + query;
+               var client = new RestClient(searchUri);
+               var request = new RestRequest();
+
+                //Parse from the regular google html response:
+                var result = client.Execute(request);
+               Regex regex = new Regex("\"resultStats\">About ([0-9,\\,]+) results</div>");
+               Match match = regex.Match(result.Content);
+               var totalResults = long.Parse(match.Groups[1].Value.Replace(",", ""));
+
+               return totalResults;
+        }
+    }
+}
---
+++ FindAnswer/OcrClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Vision.V1;
+using Grpc.Auth;
+
+namespace FindAnswerCore
+{
+    public static class OcrClient
+    {
+        public static string Recognize(string imagePath)
+        {
+            var credential = GoogleCredential.FromFile("/Users/slav/Downloads/My Project-d1092d64586a.json")
+                .CreateScoped(ImageAnnotatorClient.DefaultScopes);
+            var channel = new Grpc.Core.Channel(
+                ImageAnnotatorClient.DefaultEndpoint.ToString(),
+                credential.ToChannelCredentials());
+            
+            var client = ImageAnnotatorClient.Create(channel);
+            var image = Image.FromFile(imagePath);
+            var response = client.DetectText(image);
+            int count = response.Count;
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    return annotation.Description;
+            }
+            return null;
+        }
+    }
+}
