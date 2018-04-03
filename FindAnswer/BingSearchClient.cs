34465319e019a90033c739379dbb2cbc1f1efb74
Slav Bukhal <sbukhal@gcmlp.com>
2018-04-03 17:42:40 -0500

    start

---
+++ FindAnswer/BingSearchClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace FindAnswer
+{
+    public class BingSearchClient
+    {
+        const string BaseUri = "https://api.cognitive.microsoft.com/bing/v7.0/search";
+        const string AccessKey = "51f488e4f9fc402b948d284653925471";
+
+        public BingSearchClient()
+        {
+        }
+
+        public SearchResult Search(string query)
+        {
+            // Construct the URI of the search request
+            var uriQuery = BaseUri + "?q=" + Uri.EscapeDataString(query);
+
+            // Perform the Web request and get the response
+            WebRequest request = HttpWebRequest.Create(uriQuery);
+            request.Headers["Ocp-Apim-Subscription-Key"] = AccessKey;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+            string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            // Create result object for return
+            var searchResult = new SearchResult()
+            {
+                jsonResult = json,
+                relevantHeaders = new Dictionary<String, String>()
+            };
+
+            // Extract Bing HTTP headers
+            foreach (String header in response.Headers)
+            {
+                if (header.StartsWith("BingAPIs-") || header.StartsWith("X-MSEdge-"))
+                    searchResult.relevantHeaders[header] = response.Headers[header];
+            }
+
+            return searchResult;
+        }
+
+        public struct SearchResult
+        {
+            public String jsonResult;
+            public Dictionary<String, String> relevantHeaders;
+        }
+    }
+}
---
+++ Testing/BingClientTests.cs
@@ -0,0 +1,25 @@
+using FindAnswer;
+using Xunit;
+
+namespace Testing
+{
+    public class BingClientTests
+    {
+        private BingSearchClient _sut;
+
+//        [SetUp]
+        public void SetUp()
+        {
+            _sut = new BingSearchClient();
+        }
+
+        [Fact]
+        public void CanSearch()
+        {
+            SetUp();
+            _sut = new BingSearchClient();
+            var result = _sut.Search("hello");
+            Assert.NotNull(result);
+        }
+    }
+}
